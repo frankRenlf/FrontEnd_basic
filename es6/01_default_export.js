b8d43863cd4fb95b545081dee92e3c5dd8f95f90
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-28 22:38:38 +0800

    add

---
+++ es6/01_default_export.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var a = 10;
+var b = 20;
+function sum(x, y) {
+    return x + y;
+}
+exports["default"] = {
+    a: a,
+    sum: sum
+};
---
+++ es6/02_import.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var _01_default_export_1 = require("./01_default_export");
+console.log(_01_default_export_1["default"].a);
