d48bbc511e2b89a1e0e9151ed69b91afa10e5323
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-12-31 16:47:11 +0800

    add

--- es6/11_async_await.js
+++ es6/11_async_await.js
@@ -8,14 +8,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import thenFS from "then-fs";
+console.log('A');
 function getAllFiles() {
     return __awaiter(this, void 0, void 0, function* () {
+        console.log('B');
         const r1 = yield thenFS.readFile('./02_import.ts', 'utf8');
-        console.log(r1);
         const r2 = yield thenFS.readFile('./07_then-fs_promise.js', 'utf8');
-        console.log(r2);
         const r3 = yield thenFS.readFile('./test.js', 'utf8');
-        console.log(r3);
+        console.log(r1, r2, r3);
+        console.log('D');
     });
 }
 getAllFiles();
+console.log('C');
--- es6/11_async_await.ts
+++ es6/11_async_await.ts
@@ -1,12 +1,13 @@
 import thenFS from "then-fs"
-
+console.log('A')
 async function getAllFiles() {
+    console.log('B')
     const r1 = await thenFS.readFile('./02_import.ts', 'utf8')
-    console.log(r1)
     const r2 = await thenFS.readFile('./07_then-fs_promise.js', 'utf8')
-    console.log(r2)
     const r3 = await thenFS.readFile('./test.js', 'utf8')
-    console.log(r3)
+    console.log(r1,r2,r3)
+    console.log('D')
 }
 
 getAllFiles();
+console.log('C')
